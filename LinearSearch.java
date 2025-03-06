f14aa23d7096661b554005de0b4ccdb4cae006cc
Hari006578 <krishnahari45688@gmail.com>
2025-03-06 09:29:55 +0000

    SUBMIT

--- LinearSearch.java
+++ LinearSearch.java
@@ -13,7 +13,7 @@ public static int linearSearch(int[] arr, int target)
     public static void main(String[] args) {
      Scanner scanner = new Scanner(System.in);
      // Input array size
-      System.out.println("Enter the number of elements: ");
+      System.out.print("Enter the number of elements: ");
               int n = scanner.nextInt();
         int[] arr = new int[n];
 
